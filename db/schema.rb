83ac1b3c561a83495f51a933e89e7198dabf15e3
Skylar Grammas <skylar.grammas@fullmeasureed.com>
2019-10-01 15:52:19 +0000

    Sky branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_190439) do
+ActiveRecord::Schema.define(version: 2019_10_01_153046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2019_09_30_190439) do
     t.string "term"
     t.string "instructor"
     t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "call"
+    t.boolean "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
