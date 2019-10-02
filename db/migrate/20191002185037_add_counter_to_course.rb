5cea5a75b24c3415f397e35527a96efcf0422739
Logan Voorhees <loganvoorhees@cox.net>
2019-10-02 19:58:50 +0000

    Logan - course card/syllabus updates

--- db/migrate/20191002185037_add_counter_to_course.rb
+++ db/migrate/20191002185037_add_counter_to_course.rb
@@ -1,5 +1,5 @@
 class AddCounterToCourse < ActiveRecord::Migration[6.0]
   def change
-    add_column :courses, :syllabuses_count, :integer
+    add_column :courses, :syllabuses_count, :integer, default: 0
   end
 end
