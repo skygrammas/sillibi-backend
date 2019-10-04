c33e3bea1aec84f3255747f3b4895525831ce6da
Logan Voorhees <loganvoorhees@cox.net>
2019-10-04 15:26:06 -0400

    fixed associations, added view all assignments and user avatar

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,3 +1,14 @@
 class AssignmentsController < ActiveRecordApi::Rest::Controller
+  before_action :doorkeeper_authorize!
 
+  def index
+    @models_full_results = current_resource_owner.assignments.limit(100) unless params[:course_id]
+    super
+  end
+
+  private
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
